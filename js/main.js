5387ed39c838944e1d79580187fd8f3976a88bca
samiul19 <mdfarhansadiq01@gmail.com>
2022-12-03 20:28:04 +0600

    project

--- js/main.js
+++ js/main.js
@@ -447,17 +447,4 @@ $(function() {
    return percentage / 100 * 360
 
  }
-
 });
-
-// const filled = document.querySelector('.filled');
-
-// function update()
-// {
-//    filled.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`;
-
-//    requestAnimationFrame(update);
-// }
-
-
-// update();
